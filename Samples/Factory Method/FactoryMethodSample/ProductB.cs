364ea570b946eae466377de374dca6a1e194300c
Anand Mohan Awasthi <v-anawa@microsoft.com>
2021-08-15 19:57:42 +0530

    Factory Method

---
+++ Samples/Factory Method/FactoryMethodSample/ProductB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodSample
+{
+    class ProductB : IProduct
+    {
+        public string Operation()
+        {
+            return "Operation from ProductB";
+        }
+    }
+}
