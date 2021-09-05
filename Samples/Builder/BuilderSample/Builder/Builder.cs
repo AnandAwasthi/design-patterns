9dc40cff3c2dd92f70c1e76b4b6ecc27eb9a0b8c
Anand Mohan Awasthi <anand.awasthi.csharp@gmail.com>
2021-09-05 23:32:10 +0530

    Builder

---
+++ Samples/Builder/BuilderSample/Builder/Builder.cs
@@ -0,0 +1,56 @@
+﻿using BuilderSample.Product;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderSample
+{
+    public class Builder : IBuilder
+    {
+        private House _house = new House();
+
+        public Builder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this._house = new House();
+        }
+        public void Ceiling()
+        {
+            _house.Add("Ceiling");
+        }
+
+        public void Floor()
+        {
+            _house.Add("Floor");
+        }
+
+        public void Fountain()
+        {
+            _house.Add("Fountain");
+        }
+
+        public void Garden()
+        {
+            _house.Add("Garden");
+        }
+
+        public void Pool()
+        {
+            _house.Add("Pool");
+        }
+
+        public void Wall()
+        {
+            _house.Add("Wall");
+        }
+
+        public string GetProduct()
+        {
+            return _house.Execute();
+        }
+    }
+}
---
+++ Samples/Builder/BuilderSample/Director/Director.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderSample
+{
+    public class Director
+    {
+        private IBuilder builder;
+
+        public IBuilder Builder
+        {
+            set { builder = value; }
+        }
+
+        public void BuildHouseWithPool()
+        {
+            builder.Wall();
+            builder.Ceiling();
+            builder.Floor();
+            builder.Pool();
+        }
+
+        public void BuildHouseWithGardenAndPool()
+        {
+            builder.Wall();
+            builder.Ceiling();
+            builder.Floor();
+            builder.Pool();
+            builder.Garden();
+        }
+
+        public void BuildHouseWithFountain()
+        {
+            builder.Wall();
+            builder.Ceiling();
+            builder.Fountain();
+           
+        }
+
+    }
+}
